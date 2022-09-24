d967965a00f6dd7a37e003ec5f1d583c59d0c17e
Douglas Bates <dmbates@gmail.com>
2022-09-24 09:03:21 -0500

    Update src/hermitian.jl
    assignment outside condition
    
    Co-authored-by: Andreas Noack <andreas@noack.dk>

--- src/hermitian.jl
+++ src/hermitian.jl
@@ -21,7 +21,10 @@ function Ac_mul_A_RFP(A::Matrix{T}, uplo = :U) where {T<:BlasFloat}
     n = size(A, 2)
     par = Matrix{T}(undef, _parentsize(n))
     tr = T <: Complex ? 'C' : 'T'
-    (ul = first(string(uplo))) ∈ "UL" || throw(ArgumentError("uplo must be either :L or :U"))
+    ul = first(string(uplo))
+    if ul ∉ "UL"
+        throw(ArgumentError("uplo must be either :L or :U"))
+    end
     return HermitianRFP(LAPACK_RFP.sfrk!('N', ul, tr, 1.0, A, 0.0, par), 'N', ul)
 end
 
